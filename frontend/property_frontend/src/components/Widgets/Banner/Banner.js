77b52366581db5773ed1c63f534ab32353258d87
dela <princefold@gmail.com>
2021-07-13 19:46:34 +0000

    Ads page: added Search widget

--- frontend/property_frontend/src/components/Widgets/Banner/Banner.js
+++ frontend/property_frontend/src/components/Widgets/Banner/Banner.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TextField from "../../FormControls/TextField";
+import search_icon from "../../../assets/search_wht.svg";
 import "./banner.css";
 export default function Banner() {
   return (
@@ -8,7 +9,10 @@ export default function Banner() {
         <p>APARTMENTS, HOTELS, HOSTELS, STORES, WAREHOUSES, ETC</p>
         <p>FIND APARTMENT LISTINGS WITHOUT A SWEAT</p>
         <div className="search-box">
-          <TextField />
+          <TextField placeholder="Search by keyword" classess="search" />
+          <button className="icon-btn">
+            <img src={search_icon} alt="search" />
+          </button>
         </div>
       </div>
     </div>
